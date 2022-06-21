6238b293b13a0702c10513192cb8147ddcfe2b56
«Тумар <otumar@yandex.ru>
2022-06-22 00:49:53 +0300

    Task 02

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int Prompt(string massage)
+{
+    System.Console.Write(massage);
+    string param = Console.ReadLine();
+    return int.Parse(param);
+}
+int number = Prompt("Введите число: ");
+
+int sum = 0;
+while (number > 0) 
+{
+    sum = sum + number % 10; 
+    number = number/10; 
+}
+System.Console.WriteLine (sum);
